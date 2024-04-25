1a7e8ce4903cffc3d784a5d98d6d919337feed70
mgillig <47463178+mgillig@users.noreply.github.com>
2024-04-24 20:47:03 -0500

    completed menu

---
+++ js/dishes.js
@@ -0,0 +1,78 @@
+var apps = [
+    {
+        Image: "images/dishImages/testapp1.png",
+        Title: "Test App 1",
+        Description: "Test App 1"
+    },
+    {
+        Image: "images/dishImages/testapp2.png",
+        Title: "Test App 2",
+        Description: "Test App 2"
+    },
+    {
+        Image: "images/dishImages/testapp3.png",
+        Title: "Test App 3",
+        Description: "Test App 3"
+    },
+    {
+        Image: "images/dishImages/testapp4.png",
+        Title: "Test App 4",
+        Description: "Test App 4"
+    },
+    {
+        Image: "images/dishImages/testapp5.png",
+        Title: "Test App 5",
+        Description: "Test App 5"
+    },
+];
+var soups = [
+    {
+        Image: "images/dishImages/testsoup1.png",
+        Title: "Test Soup 1",
+        Description: "Test Soup 1"
+    },
+    {
+        Image: "images/dishImages/testsoup2.png",
+        Title: "Test Soup 2",
+        Description: "Test Soup 2"
+    },
+    {
+        Image: "images/dishImages/testsoup3.png",
+        Title: "Test Soup 3",
+        Description: "Test Soup 3"
+    },
+];
+var entrees = [
+    {
+        Image: "images/dishImages/testentree1.png",
+        Title: "Test Entree 1",
+        Description: "Test Entree 1"
+    },
+    {
+        Image: "images/dishImages/testentree2.png",
+        Title: "Test Entree 2",
+        Description: "Test Entree 2"
+    },
+    {
+        Image: "images/dishImages/testentree3.png",
+        Title: "Test Entree 3",
+        Description: "Test Entree 3"
+    },
+];
+var desserts = [
+    {
+        Image: "images/dishImages/testdessert1.png",
+        Title: "Test Dessert 1",
+        Description: "Test Dessert 1"
+    },
+    {
+        Image: "images/dishImages/testdessert2.png",
+        Title: "Test Dessert 2",
+        Description: "Test Dessert 2"
+    },
+    {
+        Image: "images/dishImages/testdessert3.png",
+        Title: "Test Dessert 3",
+        Description: "Test Dessert 3"
+    },
+];
